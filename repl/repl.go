2ea0b98738ff57426b13693cb185b602df2404fb
Ryo Suzuki <ryo.s12330@gmail.com>
2023-08-29 09:29:24 +0900

    create evaluator and object

--- repl/repl.go
+++ repl/repl.go
@@ -4,9 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/evaluator"
 	"monkey/lexer"
 	"monkey/parser"
-	"monkey/token"
 )
 
 const PROMTP = ">>"
@@ -38,11 +38,10 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
 		}
 	}
 }
